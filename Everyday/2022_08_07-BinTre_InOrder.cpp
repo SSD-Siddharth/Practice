39d98437d1ca20060a0293a59d6214201c355090
Siddharth Shankar Das <72315144+SSD-Siddharth@users.noreply.github.com>
2022-08-07 23:33:06 +0530

    Add files via upload

---
+++ Everyday/2022_08_07-BinTre_InOrder.cpp
@@ -0,0 +1,81 @@
+/*
+Binary Tree Inorder Traversal
+
+
+Given the root of a binary tree, return the inorder traversal of its nodes' values.
+
+ 
+
+Example 1:
+
+
+Input: root = [1,null,2,3]
+Output: [1,3,2]
+Example 2:
+
+Input: root = []
+Output: []
+Example 3:
+
+Input: root = [1]
+Output: [1]
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 100].
+-100 <= Node.val <= 100
+ 
+
+Follow up: Recursive solution is trivial, could you do it iteratively?
+
+*/
+
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        /*
+        Algo
+        inOrderT(root)
+            if root equals null
+                return
+            else
+                inorderTraversal(root.left)
+                print(root)
+                inOrderTrav(root.right)
+        */
+        
+        vector<int> result;
+        In_tree(root, result);
+        return result;
+    }
+    void In_tree(TreeNode *root, vector<int> &v){
+            // 1) if node is null, return;
+            if(root == nullptr) 
+                return;
+
+            // 2) visit left , output the val, then visit right; 
+            In_tree(root->left, v);
+            v.push_back(root->val);
+            In_tree(root->right, v);
+            /*
+                Left Subtree
+               Root
+               Right Subtree
+            */
+        
+    }
+};
