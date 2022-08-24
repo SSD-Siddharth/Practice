c587151441f640f133971bafed97ee931a0a4676
Siddharth Shankar Das <72315144+SSD-Siddharth@users.noreply.github.com>
2022-08-24 21:53:33 +0530

    Update Reversing_Array.cpp

--- DSA_BasicToImpCodes/Reversing_Array.cpp
+++ DSA_BasicToImpCodes/Reversing_Array.cpp
@@ -2,22 +2,29 @@
 
 #include <iostream>
 using namespace std;
-
+/*
+Input format, 
+No. of test cases, t
+	For each test case t, no. of element in the array, n
+		Elements of the array
+*/
 int main() {
 	
 	int t;
 	cin>>t;
 	while(t--){
-    	int n;
-    	cin>>n;
-    	int l[n];
-    	for (int i=0; i<n; i++){
-    	    cin>>l[i];
-    	}
-    	for(int i=n-1; i>=0; i--){
-    	    cout<<l[i]<<" ";
-    	}
-    	cout<<endl;
+		int n;
+		cin>>n;
+		int l[n];
+		//Getting the elements
+		for (int i=0; i<n; i++){
+		    cin>>l[i];
+		}
+		//We can also use swap, to swap elements till mid, and then print the array.
+		for(int i=n-1; i>=0; i--){
+		    cout<<l[i]<<" ";
+		}
+		cout<<endl;
 	}
 	return 0;
 }
